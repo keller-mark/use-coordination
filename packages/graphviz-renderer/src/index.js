ec7f2139a316ba32f44d30e0f3aea1bb0f52b6ed
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2024-01-23 15:56:25 -0500

    Improvements

--- packages/graphviz-renderer/src/index.js
+++ packages/graphviz-renderer/src/index.js
@@ -1,61 +1,41 @@
 import * as graphviz from 'graphviz';
 
-const cTypeStyle = { "color": "blue" };
 const cScopeStyle = { "color": "green" };
 const cValueStyle = { "color": "red" };
 const viewStyle = { "color": "yellow" };
 
-function addCtypeNode(g, cType) {
-  const cTypeNode = g.addNode(`cType_${cType}`, { ...cTypeStyle, label: cType });
-  //g.addEdge("coordinationSpace", cTypeNode);
-}
-
-function addCscopeNode(outerG, cType, cScope, cValue) {
-  const cScopeCluster = outerG.addCluster(`cluster_cType_${cType}_cScope_${cScope}`);
-  const g = cScopeCluster;
+function addCscopeNode(g, cType, cScope, cValue) {
 
   const cScopeNode = g.addNode(`cType_${cType}_cScope_${cScope}`, { ...cScopeStyle, label: cScope });
   const cValueNode = g.addNode(`cType_${cType}_cScope_${cScope}_value`, { ...cValueStyle, label: cValue });
-  g.addEdge(cScopeNode, `cType_${cType}`, { ltail: `cluster_cType_${cType}_cScope_${cScope}` });
   g.addEdge(cScopeNode, cValueNode);
+
 }
 
 function addViewNode(g, view) {
-  const viewNode = g.addNode(`view_${view}`, { ...viewStyle, label: view });
-  //g.addEdge("viewCoordination", viewNode);
+  g.addNode(`view_${view}`, { ...viewStyle, label: view });
 }
 
 function addViewScopeEdge(g, view, cType, cScope) {
-
-  g.addEdge(`view_${view}`, `cType_${cType}_cScope_${cScope}`, { lhead: `cluster_cType_${cType}_cScope_${cScope}` });
-
+  g.addEdge(`view_${view}`, `cType_${cType}_cScope_${cScope}`);
 }
 
 
 export function grammarToGraphviz(config) {
-  // Create digraph G
   const g = graphviz.digraph("G");
-  g.set("compound", true);
   g.set("rankdir", "LR");
 
-  const coordinationSpace = g.addCluster("cluster_coordinationSpace");
-  const viewCoordination = g.addCluster("cluster_viewCoordination");
-  
-  //coordinationSpace.addNode("coordinationSpace", { label: "coordinationSpace" });
-  //viewCoordination.addNode("viewCoordination", { label: "viewCoordination" });
-
   Object.entries(config.coordinationSpace).forEach(([cType, cObj]) => {
-
-    const cObjCluster = coordinationSpace.addCluster(`cluster_cType_${cType}`);
-    addCtypeNode(cObjCluster, cType);
+    const cTypeCluster = g.addCluster(`cluster_cType_${cType}`);
+    cTypeCluster.set("label", cType);
     
     Object.entries(cObj).forEach(([cScope, cValue]) => {
-      addCscopeNode(cObjCluster, cType, cScope, cValue);
+      addCscopeNode(cTypeCluster, cType, cScope, cValue);
     });
   });
 
   Object.entries(config.viewCoordination).forEach(([view, viewObj]) => {
-    addViewNode(viewCoordination, view);
+    addViewNode(g, view);
 
     Object.entries(viewObj.coordinationScopes).forEach(([cType, cScope]) => {
       addViewScopeEdge(g, view, cType, cScope);
