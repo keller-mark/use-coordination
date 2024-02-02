1ea7ee808c42477888b275fad76acad74779aba8
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2024-02-02 11:22:02 -0500

    Spaces

--- packages/provider/src/generic-types.ts
+++ packages/provider/src/generic-types.ts
@@ -1,7 +1,7 @@
 type CoordinationSpace = {
-	[coordinationScopeName: string]: {
-		[coordinationScope: string]: unknown;
-	};
+  [coordinationScopeName: string]: {
+    [coordinationScope: string]: unknown;
+  };
 };
 
 type CoordinationScopesMapping<T extends CoordinationSpace> = {
@@ -17,80 +17,80 @@ type CoordinationScopesByMapping<T extends CoordinationSpace> = {
 };
 
 type ViewCoordination<T extends CoordinationSpace> = {
-	[viewId: string]: {
-		coordinationScopes?: CoordinationScopesMapping<T>;
+  [viewId: string]: {
+    coordinationScopes?: CoordinationScopesMapping<T>;
     coordinationScopesBy?: CoordinationScopesByMapping<T>;
-	};
+  };
 };
 
 type Config<
-	CoordinationSpaceT extends CoordinationSpace,
-	ViewCoordinationT extends ViewCoordination<CoordinationSpaceT>,
+  CoordinationSpaceT extends CoordinationSpace,
+  ViewCoordinationT extends ViewCoordination<CoordinationSpaceT>,
 > = {
-	key?: string | number;
-	coordinationSpace?: CoordinationSpaceT;
-	viewCoordination?: ViewCoordinationT;
+  key?: string | number;
+  coordinationSpace?: CoordinationSpaceT;
+  viewCoordination?: ViewCoordinationT;
 };
 
 export function defineConfig<
-	CoordinationSpaceT extends CoordinationSpace,
-	const ViewCoordinationT extends ViewCoordination<CoordinationSpaceT>,
+  CoordinationSpaceT extends CoordinationSpace,
+  const ViewCoordinationT extends ViewCoordination<CoordinationSpaceT>,
 >(
-	config: Config<CoordinationSpaceT, ViewCoordinationT>,
+  config: Config<CoordinationSpaceT, ViewCoordinationT>,
 ): Config<CoordinationSpaceT, ViewCoordinationT> {
   return config;
 }
 
 type ResolveCoordinationScope<
-	CoordinationScopeT,
-	CoordinationSpaceT extends CoordinationSpace,
-	ViewCoordinationT extends ViewCoordination<
-		CoordinationSpaceT
-	>[string]["coordinationScopes"],
+  CoordinationScopeT,
+  CoordinationSpaceT extends CoordinationSpace,
+  ViewCoordinationT extends ViewCoordination<
+    CoordinationSpaceT
+  >[string]["coordinationScopes"],
 > = CoordinationScopeT extends
-	keyof ViewCoordinationT & keyof CoordinationSpaceT
-	? CoordinationSpaceT[CoordinationScopeT][
-		& ViewCoordinationT[CoordinationScopeT]
-		& keyof CoordinationSpaceT[CoordinationScopeT]
-	]
-	: never;
+  keyof ViewCoordinationT & keyof CoordinationSpaceT
+  ? CoordinationSpaceT[CoordinationScopeT][
+    & ViewCoordinationT[CoordinationScopeT]
+    & keyof CoordinationSpaceT[CoordinationScopeT]
+  ]
+  : never;
 
 type UseCoordination<
-	CoordinationSpaceT extends CoordinationSpace,
-	ViewCoordinationT extends ViewCoordination<CoordinationSpaceT>,
+  CoordinationSpaceT extends CoordinationSpace,
+  ViewCoordinationT extends ViewCoordination<CoordinationSpaceT>,
 > = {
-	<
-		ViewId extends keyof ViewCoordinationT,
-		const CoordinationScopes extends Array<
-			keyof ViewCoordinationT[ViewId]["coordinationScopes"]
-		>,
-	>(viewId: ViewId, scope: CoordinationScopes): [
-		{
-			[Scope in CoordinationScopes[number]]: ResolveCoordinationScope<
-				Scope,
-				CoordinationSpaceT,
-				ViewCoordinationT[ViewId]["coordinationScopes"]
-			>;
-		},
-		{
-			[
-				Scope in CoordinationScopes[number] as `set${Capitalize<
-					Scope & string
-				>}`
-			]: (
-				update: ResolveCoordinationScope<
-					Scope,
-					CoordinationSpaceT,
-					ViewCoordinationT[ViewId]["coordinationScopes"]
-				>,
-			) => void;
-		},
-	];
+  <
+    ViewId extends keyof ViewCoordinationT,
+    const CoordinationScopes extends Array<
+      keyof ViewCoordinationT[ViewId]["coordinationScopes"]
+    >,
+  >(viewId: ViewId, scope: CoordinationScopes): [
+    {
+      [Scope in CoordinationScopes[number]]: ResolveCoordinationScope<
+        Scope,
+        CoordinationSpaceT,
+        ViewCoordinationT[ViewId]["coordinationScopes"]
+      >;
+    },
+    {
+      [
+        Scope in CoordinationScopes[number] as `set${Capitalize<
+          Scope & string
+        >}`
+      ]: (
+        update: ResolveCoordinationScope<
+          Scope,
+          CoordinationSpaceT,
+          ViewCoordinationT[ViewId]["coordinationScopes"]
+        >,
+      ) => void;
+    },
+  ];
 };
 
 declare function createUseCoordination<
-	CoordinationSpaceT extends CoordinationSpace,
-	ViewCoordinationT extends ViewCoordination<CoordinationSpaceT>,
+  CoordinationSpaceT extends CoordinationSpace,
+  ViewCoordinationT extends ViewCoordination<CoordinationSpaceT>,
 >(
-	config: Config<CoordinationSpaceT, ViewCoordinationT>,
+  config: Config<CoordinationSpaceT, ViewCoordinationT>,
 ): UseCoordination<CoordinationSpaceT, ViewCoordinationT>;
