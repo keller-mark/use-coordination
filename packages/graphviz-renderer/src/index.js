b008e128aad1d896012e99e3f1b729e66dd93204
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2024-02-03 10:34:10 -0500

    Different graphviz library

--- packages/graphviz-renderer/src/index.js
+++ packages/graphviz-renderer/src/index.js
@@ -1,34 +1,43 @@
-import * as graphviz from 'graphviz';
+import { attribute as _, Digraph, Subgraph, Node, Edge, toDot } from 'ts-graphviz';
 
-const cScopeStyle = { "color": "green" };
-const cValueStyle = { "color": "red" };
-const viewStyle = { "color": "yellow" };
+const cScopeStyle = { [_.color]: "green" };
+const cValueStyle = { [_.color]: "red" };
+const viewStyle = { [_.color]: "yellow" };
 
 function addCscopeNode(g, cType, cScope, cValue) {
-  const cScopeNode = g.addNode(`cType_${cType}_cScope_${cScope}`, { ...cScopeStyle, label: cScope });
-  const cValueNode = g.addNode(`cType_${cType}_cScope_${cScope}_value`, { ...cValueStyle, label: cValue });
-  g.addEdge(cScopeNode, cValueNode);
-
+  const cScopeNode = new Node(`cType_${cType}_cScope_${cScope}`, {
+    ...cScopeStyle,
+    [_.label]: cScope,
+  })
+  g.addNode(cScopeNode);
+  const cValueNode = new Node(`cType_${cType}_cScope_${cScope}_value`, {
+    ...cValueStyle,
+    [_.label]: JSON.stringify(cValue),
+  });
+  g.addNode(cValueNode);
+  g.addEdge(new Edge([cScopeNode, cValueNode]));
 }
 
 function addViewNode(g, view) {
-  g.addNode(`view_${view}`, { ...viewStyle, label: view });
+  g.addNode(new Node(`view_${view}`, { ...viewStyle, [_.label]: view }));
 }
 
 function addViewScopeEdge(g, view, cType, cScope) {
-  g.addEdge(`view_${view}`, `cType_${cType}_cScope_${cScope}`);
+  g.addEdge(new Edge([new Node(`view_${view}`), new Node(`cType_${cType}_cScope_${cScope}`)]));
 }
 
 // TODO: implement using TS
 export function toGraphviz(config) {
-  const g = graphviz.digraph("G");
-  g.set("rankdir", "LR");
-  
+  const g = new Digraph({
+    [_.rankdir]: "LR",
+  });  
   // TODO: handle meta-coordination, multi-coordination, and multi-level coordination.
 
   Object.entries(config.coordinationSpace).forEach(([cType, cObj]) => {
-    const cTypeCluster = g.addCluster(`cluster_cType_${cType}`);
-    cTypeCluster.set("label", cType);
+    const cTypeCluster = new Subgraph(`cluster_cType_${cType}`, {
+      [_.label]: cType,
+    });
+    g.addSubgraph(cTypeCluster);
     
     Object.entries(cObj).forEach(([cScope, cValue]) => {
       addCscopeNode(cTypeCluster, cType, cScope, cValue);
@@ -43,5 +52,5 @@ export function toGraphviz(config) {
     });
   });
 
-  return g.to_dot();
+  return toDot(g);
 }
