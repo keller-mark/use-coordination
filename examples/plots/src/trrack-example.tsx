bcdb15f38fd94b815df494509e2d6855fe0df3e8
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2024-02-08 17:08:52 -0500

    Enable/disable undo/redo buttons depending on whether it is possible.

--- examples/plots/src/trrack-example.tsx
+++ examples/plots/src/trrack-example.tsx
@@ -1,8 +1,6 @@
-import React, { useState, useEffect, useMemo, useCallback, useRef } from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 import {
   CoordinationProvider,
-  ZodErrorBoundary,
-  useCoordination,
   defineConfig,
 } from '@use-coordination/all';
 import { z } from 'zod';
@@ -10,7 +8,7 @@ import {
   initializeTrrack,
   Registry,
 } from '@trrack/core';
-import { cloneDeep, set } from 'lodash-es';
+import { cloneDeep } from 'lodash-es';
 import { letterFrequency } from '@visx/mock-data';
 import { VegaLitePlotView } from './vega-lite.js';
 import { D3BarPlotView } from './d3.js';
@@ -73,6 +71,8 @@ export function TrrackExample(props: any) {
   const [config, setConfig] = useState<any>(initialConfig);
   const [configFromTrrack, setConfigFromTrrack] = useState<any>(0);
   const [showTrrackConfig, setShowTrrackConfig] = useState(false);
+  const [canUndo, setCanUndo] = useState(false);
+  const [canRedo, setCanRedo] = useState(false);
   const { trrack, actions } = useTrrack();
 
   useEffect(() => {
@@ -80,17 +80,30 @@ export function TrrackExample(props: any) {
     setShowTrrackConfig(false);
   }, [config]);
 
+  useEffect(() => {
+    const unsubscribe = trrack.currentChange(() => {
+      const canNotUndo = (
+          trrack.current.id === trrack.root.id
+          || (trrack.current as any).parent === trrack.root.id
+      );
+      const canNotRedo = trrack.graph.current.children.length === 0;
+      setCanUndo(!canNotUndo);
+      setCanRedo(!canNotRedo);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, [trrack]);
+
   const onUndo = useCallback(() => {
     trrack.undo();
     setShowTrrackConfig(true);
     setConfigFromTrrack((prev: number) => prev + 1);
-    // If at the current state, then setShowTrrackConfig(false).
   }, [trrack]);
   const onRedo = useCallback(() => {
     trrack.redo();
     setShowTrrackConfig(true);
     setConfigFromTrrack((prev: number) => prev + 1);
-    // If at the current state, then setShowTrrackConfig(false).
   }, [trrack]);
 
   const configToUse = (showTrrackConfig ? trrack.getState().config : config);
@@ -104,37 +117,34 @@ export function TrrackExample(props: any) {
           flex-wrap: wrap;
         }
       `}</style>
-      <button onClick={onUndo}>Undo</button>
-      <button onClick={onRedo}>Redo</button>
-      <ZodErrorBoundary>
-        <CoordinationProvider
-          config={configToUse}
-          onConfigChange={(newConfig: any) => {
-            console.log('onConfigChange');
-            setConfig(newConfig);
-          }}
-          diffByKey={false}
-          emitInitialConfigChange={false}
-        >
-          <div className="multiplot-container">
-            <div className="plot-container">
-              <VegaLitePlotView viewUid="vegaLite" data={letterFrequency} />
-            </div>
-            <div className="plot-container">
-              <D3BarPlotView viewUid="d3" data={letterFrequency} />
-            </div>
-            <div className="plot-container">
-              <VisxPlotView viewUid="visx" data={letterFrequency} />
-            </div>
-            <div className="plot-container">
-              <PlotlyBarPlotView viewUid="plotly" data={letterFrequency} />
-            </div>
+      <button onClick={onUndo} disabled={!canUndo}>Undo</button>
+      <button onClick={onRedo} disabled={!canRedo}>Redo</button>
+      <CoordinationProvider
+        config={configToUse}
+        onConfigChange={(newConfig: any) => {
+          setConfig(newConfig);
+        }}
+        diffByKey={false}
+        emitInitialConfigChange={false}
+      >
+        <div className="multiplot-container">
+          <div className="plot-container">
+            <VegaLitePlotView viewUid="vegaLite" data={letterFrequency} />
           </div>
-        </CoordinationProvider>
-        <pre>
-          {JSON.stringify(configToUse, null, 2)}
-        </pre>
-      </ZodErrorBoundary>
+          <div className="plot-container">
+            <D3BarPlotView viewUid="d3" data={letterFrequency} />
+          </div>
+          <div className="plot-container">
+            <VisxPlotView viewUid="visx" data={letterFrequency} />
+          </div>
+          <div className="plot-container">
+            <PlotlyBarPlotView viewUid="plotly" data={letterFrequency} />
+          </div>
+        </div>
+      </CoordinationProvider>
+      <pre>
+        {JSON.stringify(configToUse, null, 2)}
+      </pre>
     </>
   );
 }
