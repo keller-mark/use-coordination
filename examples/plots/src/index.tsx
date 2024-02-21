41de59ad4bece59d71be3d36e3208fa925d587be
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2024-02-21 17:04:20 -0500

    2/3 weather plots

--- examples/plots/src/index.tsx
+++ examples/plots/src/index.tsx
@@ -1,3 +1,4 @@
 export { PlotsExample } from './plots-example.js';
 export { MultiLevelPlotsExample } from './multilevel-example.js'
 export { TrrackExample } from './trrack-example.js';
+export { WeatherExample } from './weather.js';
