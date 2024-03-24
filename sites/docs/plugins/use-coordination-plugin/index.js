c08965893402730874dd4b3860ff7db54981d62b
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2024-03-24 10:24:52 -0400

    Fix bundle (#89)
    * Use different version of loaders.gl to fix child_process import issue
    
    * Changeset

--- sites/docs/plugins/use-coordination-plugin/index.js
+++ sites/docs/plugins/use-coordination-plugin/index.js
@@ -14,6 +14,7 @@ module.exports = function(context, options) {
             fs: false,
             path: false,
             util: false,
+            child_process: false,
           },
         },
       };
